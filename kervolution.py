e26fdb5399d08518bcdc7c313dd10b345133f333
Wang, Chen <jeffsan@gmx.com>
2018-09-20 10:09:38 +0800

    fix kervolution

--- kervolution.py
+++ kervolution.py
@@ -32,12 +32,10 @@ class Kerv2d(nn.Conv2d):
     '''
     kervolution with following options:
     mapping: [translation, polar, logpolar, random]
-    kernel_type: [linear, polynomial, sigmoid, gaussian, cauchy]
+    kernel_type: [linear, polynomial, gaussian, etc.]
     default is convolution:
-             mapping     --> translation,
              kernel_type --> linear,
-    alpha is the regularizer to control kernel fitting
-    balance, power, slope, gamma is valid only when the kernel_type is specified
+    balance, power, gamma is valid only when the kernel_type is specified
     if learnable_kernel = True,  they just be the initial value of learable parameters
     if learnable_kernel = False, they are the value of kernel_type's parameter
     the parameter [power] cannot be learned due to integer limitation
@@ -46,6 +44,7 @@ class Kerv2d(nn.Conv2d):
             stride=1, padding=0, dilation=1, groups=1, bias=True,
             mapping='translation', kernel_type='linear', learnable_kernel=False, kernel_regularizer=False,
             alpha=0.03, balance=1, power=3, sigma=2, gamma=1):
+
         super(Kerv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
         self.mapping, self.kernel_type = mapping, kernel_type
         self.learnable_kernel, self.kernel_regularizer = learnable_kernel, kernel_regularizer
@@ -53,88 +52,45 @@ class Kerv2d(nn.Conv2d):
 
         # parameter for kernel type
         if learnable_kernel == True:
-            self.alpha   = nn.Parameter(torch.cuda.FloatTensor([alpha]), requires_grad=True)
-            self.balance = nn.Parameter(torch.cuda.FloatTensor([balance]), requires_grad=True)
-            self.sigma   = nn.Parameter(torch.cuda.FloatTensor([sigma]), requires_grad=True)
-            self.gamma   = nn.Parameter(torch.cuda.FloatTensor([gamma]), requires_grad=True)
-        if kernel_type == 'gaussian' or kernel_type == 'cauchy':
-            self.weight_ones = Variable(torch.cuda.FloatTensor(self.weight.size()).fill_(1/self.weight.numel()), requires_grad=False)
-
-        # # mapping functions
-        # if mapping == 'translation':
-        #     return
-        
-        # index_all = np.reshape(np.arange(self.weight.nelement()), (out_channels*in_channels, kernel_size**2))
-        # if mapping == 'polar':
-        #     import cv2
-        #     center = (kernel_size/2, kernel_size/2)
-        #     radius = (kernel_size+1) / 2.0
-        #     index = np.reshape(np.arange(kernel_size**2),(kernel_size,kernel_size))
-        #     maps =  np.reshape(cv2.linearPolar(index, center, radius, cv2.WARP_FILL_OUTLIERS).astype(int), (kernel_size**2))
-        #     index_all[:,:] = index_all[:,maps]
-        # elif mapping == 'logpolar':
-        #     import cv2
-        #     center = (kernel_size/2, kernel_size/2)
-        #     radius = (kernel_size+1) / 2.0
-        #     M = kernel_size / np.log(radius)
-        #     index = np.reshape(np.arange(kernel_size**2),(kernel_size,kernel_size))
-        #     maps =  np.reshape(cv2.logPolar(index, center, M, cv2.WARP_FILL_OUTLIERS).astype(int), (kernel_size**2))
-        #     index_all[:,:] = index_all[:,maps]
-        # elif mapping == 'random':
-        #     for i in range(out_channels*in_channels):
-        #         index_all[i,:] = index_all[i, np.random.randint(low=0, high=kernel_size**2, size=kernel_size**2)]
-        # else:
-        #     NotImplementedError()
-        # self.mapping_index = torch.cuda.LongTensor(index_all).view(-1)
+            self.balance = nn.Parameter(torch.cuda.FloatTensor([balance] * out_channels), requires_grad=True).view(-1, 1)
+            self.sigma   = nn.Parameter(torch.cuda.FloatTensor([sigma]   * out_channels), requires_grad=True).view(-1, 1)
+            self.gamma   = nn.Parameter(torch.cuda.FloatTensor([gamma]   * out_channels), requires_grad=True).view(-1, 1)
 
     def forward(self, input):
 
-        # if self.mapping == 'translation':
-        #     self.weights = self.weight
-        # else:
-        #     self.weights = self.weight.view(-1)[self.mapping_index]
-        #     self.weights = self.weights.view(self.out_channels, self.in_channels, self.kernel_size[0], self.kernel_size[1])
-        
-        minibatch, _, input_width, input_hight = input.size()
+        minibatch, in_channels, input_width, input_hight = input.size()
+        assert(in_channels == self.in_channels)
         input_unfold = F.unfold(input, kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
         input_unfold = input_unfold.view(minibatch, 1, self.kernel_size[0]*self.kernel_size[1]*self.in_channels, -1)
-        weight_flat  = self.weight.view(self.out_channels, -1)
+        weight_flat  = self.weight.view(self.out_channels, -1, 1)
         output_width = (input_width - self.kernel_size[0] + 2 * self.padding[0]) // self.stride[0] + 1
         output_hight = (input_hight - self.kernel_size[1] + 2 * self.padding[1]) // self.stride[1] + 1
 
         if self.kernel_type == 'linear':
-            # return conv2d(input, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
-            if self.bias is None:
-                output = weight_flat @ input_unfold
-            else:
-                output = weight_flat @ input_unfold + self.bias.view(self.out_channels,-1)
-            return output.view(minibatch, self.out_channels, output_width, output_hight)
+            output = conv2d(input, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
+        elif self.kernel_type == 'diff':
+            output = (input_unfold - weight_flat).sum(dim=2)
+
+        elif self.kernel_type == '1-norm':
+            output = (input_unfold - weight_flat).abs().sum(dim=2)
+
+        elif self.kernel_type == '2-norm':
+            output = ((input_unfold - weight_flat)**2).sum(dim=2)
+
+        elif self.kernel_type == '3-norm':
+            output = ((input_unfold - weight_flat)**3).sum(dim=2)
+
         elif self.kernel_type == 'polynomial':
-            outputs = (y+self.balance) ** self.power
-        elif self.kernel_type == 'sigmoid':
-            outputs = y.tanh()
+            output = ((input_unfold * weight_flat).sum(dim=2) + self.balance)**self.power
+
         elif self.kernel_type == 'gaussian':
-            input_norm = conv2d(input**2, self.weight_ones, None, self.stride, self.padding, self.dilation, self.groups)
-            weight_norm = (self.weight**2).sum(3).sum(2).sum(1).view(1,self.out_channels,1,1)/self.weight.numel()
-            weight_norm = weight_norm.expand(input_norm.size()[0],-1,input_norm.size()[2],input_norm.size()[3])
-            outputs = (-self.gamma*(weight_norm+input_norm-2*y)).exp()
-        elif self.kernel_type == 'cauchy':
-            input_norm = conv2d(input**2, self.weight_ones, None, self.stride, self.padding, self.dilation, self.groups)
-            weight_norm = (self.weight**2).sum(3).sum(2).sum(1).view(1,self.out_channels,1,1)/self.weight.numel()
-            weight_norm = weight_norm.expand(input_norm.size()[0],-1,input_norm.size()[2],input_norm.size()[3])
-            outputs = 1/(1+(weight_norm+input_norm-2*y)/(self.sigma**2))
-        else:
-            return NotImplementedError()
+            output = (-(((input_unfold - weight_flat)**2).sum(dim=2)*self.gamma)).exp()
 
-        if self.kernel_regularizer:
-            outputs = outputs + self.alpha * self.weight.abs().mean()
-        
-        return outputs
+        if self.bias is not None:
+            output += self.bias.view(self.out_channels, -1)
 
-    def parameters(self):
-        if self.learnable_kernel:
-            print('alpha: %.3f, power: %.2f, balance: %.2f, sigma: %.2f, gamma: %.2f' % (
-                self.alpha.data[0], self.power, self.balance.data[0], self.sigma.data[0], self.gamma.data[0]))
+        return output.view(minibatch, self.out_channels, output_width, output_hight)
 
 
 nn.Kerv2d = Kerv2d
@@ -145,6 +101,5 @@ if __name__ == '__main__':
                      kernel_size=3,              # filter size
                      stride=1,                   # filter movement/step
                      padding=1,                  # input padding
-                     mapping='random',           # mapping
                      kernel_type='polynomial',   # kernel type
                      learnable_kernel=True)      # enable learning parameters
